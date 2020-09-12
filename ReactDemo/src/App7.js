583eeb854c981fc94e09df298234a1ae792683b3
zhoupeilai <zhoupeilai001@163.com>
2020-09-12 10:48:19 +0800

    父子组件优化

--- ReactDemo/src/App7.js
+++ ReactDemo/src/App7.js
@@ -2,11 +2,18 @@
 import React, { Component } from 'react'
 
 class Header extends Component{    //子组件
+    static defaultProps = {    //给props添加默认属性
+        bgc: 'skyBlue',
+        title: '默认标题'
+    }
+
 
     render() {
         return (
             <div style={{width:"100%", height:40, lineHeight:"40px", backgroundColor: this.props.bgc}}>
                 {this.props.title}
+                {/* 获取父元素中的子元素值 */}
+                {this.props.children}    
             </div>
         )
     }
@@ -24,7 +31,8 @@ export default class App7 extends Component {   //父组件
         return (
             <div>
                 <Header title={this.state.title} bgc="#cff"></Header>
-                <Header title="列表页标题" bgc="pink"></Header>
+                <Header title="列表页标题"></Header>
+                <Header>子元素</Header>
             </div>
         )
     }
