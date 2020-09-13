4b039b8212aa9e73e0341ccf946eea1b8f68d6d7
zhoupeilai <zhoupeilai001@163.com>
2020-09-13 22:58:11 +0800

    React生命周期函数

--- ReactDemo/day2/src/App6.js
+++ ReactDemo/day2/src/App6.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 
-export default class App4 extends Component {
+class Child extends Component {
     // -----------------------------------------
     //第一阶段 挂载期
     constructor(props) {
@@ -23,14 +23,16 @@ export default class App4 extends Component {
     //---------------------------------------------------------------
 
     //第二阶段  更新期
-    UNSAFE_componentWillReceiveProps(){
+    UNSAFE_componentWillReceiveProps(nextProps){
         console.log("2.1  UNSAFE_componentWillReceiveProps在接收props属性之前")
+        console.log("faterNum的旧值为：", this.props.faterNum)
+        console.log("faterNum的新值为：",nextProps.faterNum)
     }
     shouldComponentUpdate(nextProps, nextState){
         console.log("2.2 shouldComponentUpdate 可以判断修改前后的值是不是一样，不一样才去执行render。减少不必要的render优化性能")
         console.log("num的旧值为：", this.state.num)
         console.log("num的新值为：", nextState.num)
-        return (this.state.num !== nextState.num)
+        return (this.state.num !== nextState.num || this.props.faterNum !== nextProps.faterNum)
     }
     UNSAFE_componentWillUpdate(){
         console.log("2.3 componentWillUpdata 更新之前执行的代码")
@@ -50,6 +52,32 @@ export default class App4 extends Component {
             <div>
                 <p>{this.state.num}</p>
                 <button onClick={this.handleClick.bind(this)}>按钮</button>
+                <p>{this.props.faterNum}</p>
+            </div>
+        )
+    }
+}
+
+
+export default class App6 extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            faterNum: 10
+        }
+    }
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({
+                faterNum: 30
+            })
+        },2000)
+    }
+    
+    render(){
+        return(
+            <div>
+                <Child faterNum={this.state.faterNum}></Child>
             </div>
         )
     }
